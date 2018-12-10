f07755bbff2d7f2c7f7840bd8ca9e1b7fbdfcc22
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-12-10 17:37:23 -0600

    added d3 to read csv

--- src/components/csv-graph/csv-graph.js
+++ src/components/csv-graph/csv-graph.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-const csv = require('csv-parser')
-const fs = require('fs')
-const results = [];
 
 class CsvGraph extends Component {
 
     render() {
-console.log(fs);
-        // fs.createReadStream('data.csv')
-        //     .pipe(csv())
-        //     .on('data', results.push)
-        //     .on('end', () => {
-        //         console.log(results);
-        //     });
+
         return (
             <div>
             </div>
