a78b26676b6e0d55a369dcb53f0606a661e69d51
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-12-12 12:11:43 -0600

    reading local csv file

--- src/components/csv-graph/csv-graph.js
+++ src/components/csv-graph/csv-graph.js
@@ -3,15 +3,26 @@ import * as d3 from 'd3';
 
 class CsvGraph extends Component {
 
+    state = {
+        data: []
+    }
+
     componentDidMount() {
-        d3.csv("/data.csv", (data) => {
-            console.log(data);
-          });
+        d3.csv("/data.csv", point => {
+            this.setState({
+                data: [...this.state.data, {
+                    first_name: point["first name"],
+                    last_name: point["last name"],
+                    occasion: point["occasion id"],
+                }]
+            });
+        });
     }
 
     render() {
         return (
             <div>
+                <button onClick={() => {console.log(this.state)}}>Log</button>
             </div>
         );
     }
