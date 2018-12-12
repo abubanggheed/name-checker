f226b8a1f941f6f2528118c152eb2a5e381ce76f
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-12-12 11:52:27 -0600

    logging out results of csv

--- src/components/csv-graph/csv-graph.js
+++ src/components/csv-graph/csv-graph.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
 
 class CsvGraph extends Component {
 
-    render() {
+    componentDidMount() {
+        d3.csv("/data.csv", (data) => {
+            console.log(data);
+          });
+    }
 
+    render() {
         return (
             <div>
             </div>
