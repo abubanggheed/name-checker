3e6d884d16bf40975d22aee6bcd7661647581437
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-12-12 14:33:20 -0600

    reading csv data from upload rather than from public file

--- src/components/csv-graph/csv-graph.js
+++ src/components/csv-graph/csv-graph.js
@@ -4,23 +4,35 @@ import * as d3 from 'd3';
 class CsvGraph extends Component {
 
     state = {
-        data: []
+        data: [],
+        upload: "/data.csv",
     }
 
-    componentDidMount() {
-        d3.csv("/data.csv", point => {
+
+    setData = path => {
+        this.setState({
+            ...this.state,
+            data: [],
+        });
+        d3.csv(path, point => {
             this.setState({
+                ...this.state,
                 data: [...this.state.data, {
                     first_name: point["first name"],
                     last_name: point["last name"],
                     occasion: point["occasion id"],
+                    role: point["role"],
                 }]
             });
         });
     }
 
-    handleNew = () => {
-        console.log('adding new')
+    handleUpload = event => {
+        this.setState({
+            ...this.state,
+            upload: event.target.value
+        })
+        this.setData(this.state.upload)
     }
 
     render() {
@@ -32,20 +44,22 @@ class CsvGraph extends Component {
                             <th>First Name</th>
                             <th>Last Name</th>
                             <th>Occasion</th>
+                            <th>Role</th>
                         </tr>
                     </thead>
                     <tbody>
                         {this.state.data.map(point => (
-                            <tr key={point.occasion}>
+                            <tr key={point.first_name + point.last_name + point.occasion}>
                                 <td>{point.first_name}</td>
                                 <td>{point.last_name}</td>
                                 <td>{point.occasion}</td>
+                                <td>{point.role}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
-                <button onClick={this.handleNew}>New</button>
                 <button onClick={() => { console.log(this.state) }}>Log</button>
+                <input type="file" filename={this.state.upload} onChange={this.handleUpload} />
             </div>
         );
     }
